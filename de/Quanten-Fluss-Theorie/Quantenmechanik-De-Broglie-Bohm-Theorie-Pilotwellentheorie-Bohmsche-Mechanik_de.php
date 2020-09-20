f0f34bf545e417c78f8d9fd7b0247834d50e113a
Wolfgang Huß <wolle.huss@pjannto.com>
2020-09-20 20:03:07 +0200

    Write on Die Realisierung der De-Broglie-Bohm-Theorie

--- de/Quanten-Fluss-Theorie/Quantenmechanik-De-Broglie-Bohm-Theorie-Pilotwellentheorie-Bohmsche-Mechanik_de.php
+++ de/Quanten-Fluss-Theorie/Quantenmechanik-De-Broglie-Bohm-Theorie-Pilotwellentheorie-Bohmsche-Mechanik_de.php
@@ -13,13 +13,13 @@
 	
           <?php To_f_Paragraph_list( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '                ',
                 array(
-                  array( 'notice', array( Display => 'vis', text => array(
+                  array( 'notice', array( Display => 'invis', text => array(
                     '• Es handelt sich um eine fraktale Quantenmechanik.',
                     '\\bold{Die Quantenmechanik ist emergent:}',
-                    '• Tim Palmer bringt in seinem Beitrag die Unentscheidbarkeit, die Unberechenbarkeit und die Vereinheltichung der Physik zusammen. Er führt darüber die Quantenphysik, die Relativitätstheorie und die Chaos-Theorie näher aneinander heran. \\jump[http://backreaction.blogspot.com/2020/02/guest-post-undecidability.html]{}{Guest Post: “Undecidability, Uncomputability and the Unity of Physics. Part 1.” by Tim Palmer}',
+                    '• Tim Palmer bringt in seinem Beitrag die Unentscheidbarkeit, die Unberechenbarkeit und die Vereinheltlichung der Physik zusammen. Er führt darüber die Quantenphysik, die Relativitätstheorie und die Chaos-Theorie näher aneinander heran. \\jump[http://backreaction.blogspot.com/2020/02/guest-post-undecidability.html]{}{Guest Post: “Undecidability, Uncomputability and the Unity of Physics. Part 1.” by Tim Palmer}',
                     '\\bold{Das Messproblem der Quantenmechanik ist ein Korrekturproblem:}',
                     '• Sabine Hossenfelder führt in ihrem Blog sehr anschaulich aus, dass das Messproblem der Quantenmechanik eine Korrektur unseres Wissens ist.\\footnote{\\const{FrQFT_g_footnote_text_QMMessproblKorrprobl}}',
-                    '\\bold{De-Broglie-Bohm-Theorie ist konsitent mit der Quantenmechanik:}',
+                    '\\bold{De-Broglie-Bohm-Theorie ist konsistent mit der Quantenmechanik:}',
                     '• Die De-Broglie-Bohm-Theorie ergibt die gleiche Wahrscheinlichkeitsstatistik, wie die Quantenmechanik.\\footnote{\\const{FrQFT_g_footnote_text_DeBroglieBohmTheorieKonsQM}}',
                     '– Dies kommt, weil die Quantengleichgewichtshypothese gilt, vgl. \\cite{Schaal:InwieweitIstEineRelativistischeFormulierungDerBohmschenMechanikMoeglich:2014}, Kap. 2. Grundkonzepte der Bohmschen Mechanik, 2.2. Quantengleichgewichtshypothese, S. 5-6, hier S. 5.',
                     '– \\quote{Die Quantengleichgewichtshypothese impliziert auch die Heisenbergsche Unschärferelation.} Vgl. \\cite{Schaal:InwieweitIstEineRelativistischeFormulierungDerBohmschenMechanikMoeglich:2014}, Kap. 2. Grundkonzepte der Bohmschen Mechanik, 2.2. Quantengleichgewichtshypothese, S. 5-6, hier S. 5. (Siehe auch: D. Dürr, S. Goldstein und N. Zanghì, Quantum Physics without Quantum Philosophy, Springer-Verlag, Berlin Heidelberg 2013. UND: D. Dürr, S. Goldstein und N. Zanghì, „Quantum Equilibrium and the Origin of Absolute Uncertainty“, Journal of Statistical Physics 67, pp. 843-907, 1992.)',
@@ -39,6 +39,80 @@
                     ))),
                       
                   array( 'text', array( text => array(
+                    'Spätestens seit Anfang 2003xxx ist mir aufgefallen, dass die Quanten-Fluss-Theorie Eigenschaften besitzt, die einen anderen Blick auf die Erscheinungen der'."\n".
+                    'Quantenmechanik eröffnen.'."\n".
+                    'Ich las damals das Buch ›Geheime Botschaften‹ von Simon Singh, in dem es auch um Quantenkryptographie\\footnote{Vgl. \\cite{Singh:GeheimeBotschaften:2020}, 8 Ein Quantensprung in die Zukunft, Quantenkryptographie, S. 400-421. \\\\ Internet: \\\\ Vgl. \\cite{wiki:Quantenkryptographie:2020}.}'."\n".
+                    'geht.\\footnote{\\cite{Singh:GeheimeBotschaften:2020}, 8 Ein Quantensprung in die Zukunft, S. 383-421.}'."\n".
+                    'Um die Erklärung der Quantenkryptographie vorzubereiten geht Singh auf die Erscheinungen der Quantenwelt, deren Erklärung durch die QuantenmechanikXXX'."\n".
+                    'und wiederum deren Interpretation ein.'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:FrQFT:De-Broglie-Bohm-Theorie:Vortext:XXX', text =>
+                      
+                'Das Doppelspaltexperiment mit einzelnen Photonen', subline =>
+                  '')),
+                  array( 'text', array( text => array(
+                    'Singh nimmt dabei das DoppelspaltexperimentXXX als exemplarisch für die spektakulären Phänomene der QuantenphysikXXX, wie es oft so üblich ist.'."\n".
+                    'Spektakulär deshalb, weil Singh schließlich die Ausführung des Experiments mit einer Quelle einzelner Photonen – Lichtteilchen – pro Zeit betrachtet.'."\n".
+                    'Er erklärt, dass sich auf dem XXX Schirm ergebene Muster unterscheidet sich auch in dem Fall nicht im Ergebnis, wenn einzelne Photonen nacheinander, also nicht zeitgleich,'."\n".
+                    'durch die beiden Spalte fliegen, vom dem Muster, wenn Myriaden gleichzeitig durch die Spalte fliegen und auf den Schirm treffen.'."\n",
+                      'Bei den Myriaden gleichzeitiger Photonen wird argumentiert, sie würden  einander in Interferenz überlagern.'."\n".
+                    'Genauer gesagt, durch ihre Welleneigenschaften würden sich, entsprechend der Quantenmechanik, die Wellen ihrer statistischen Aufenthaltswahrscheinlichkeiten überlagern,'."\n".
+                    'Wodurch sich das Interferenzmuster auf dem Schirm ergibt.'."\n",
+                      'Bei einzelnen Photonen sei dies nicht anders, obwohl die späteren Photonen noch nicht einmal im Glühdraht der Lampe entstanden sind, erklärt die heutige Physik.XXX'."\n".
+                    'Das fand ich doch schon sehr mysteriös.'."\n".
+                    'Zumal das Wahrscheinlichkeits-Interferenzmuster, nach langläufiger Ansicht der Physiker, nichts mit realen physikalischen Erscheinungen zu tun haben soll.'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:FrQFT:De-Broglie-Bohm-Theorie:Vortext:XXX', text =>
+                      
+                'Eine grundsätzlich andere Lösung des Problems', subline =>
+                  'Ein Interferenzmuster im Vakuum')),
+                  array( 'text', array( text => array(
+                    'Ich stellte mir die Frage, wie es wohl wäre, wenn Informationen über den Aufbau des Experiments an jeder Stelle im Raum bekannt wären.'."\n".
+                    'Mir kam das Bild eines Interferenzmusters in der Struktur des Vakuums zwischen der Spaltblende und dem Schirm (siehe XXX).'."\n".
+                    'Dieses sollte mit der Existenz der Lichtteilchen nichts zu tun haben, außer, dass die Frequenz ähnlich wäre, bestimmt durch den experimentellen Aufbau.'."\n",
+                      'In meiner Fantasie wurde es durch die Bestandteile des Vakuums selber und durch die Struktur der Doppelspaltblende erzeugt.'."\n".
+                    'Dieses Muster sollte dann die Bahnen der Photonen beeinflussen.'."\n".
+                    'Ich wusste nicht genau, wie das funktionieren sollte, aber war von meinem Konzept begeistert.'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:FrQFT:De-Broglie-Bohm-Theorie:Vortext:XXX', text =>
+                      
+                'Die De-Broglie-Bohm-Theorie', subline =>
+                  'Ein spätes Aha-Erlebnis')),
+                  array( 'text', array( text => array(
+                    'Es war mir zu dem Zeitpunkt nicht bekannt, dass es die De-Broglie-Bohm-Theorie gab, die im Grunde nach dem gleichen Prinzip funktioniert.'."\n".
+                    'Denn ich kümmerte mich in der Zwischenzeit unter anderem um die Struktur der Elementarteilchen, um die Beschreibung des Gravitationspotenzials,'."\n".
+                    'um den Mechanismus der Quantengravitation und um vieles andere mehr.'."\n",
+                      'Erst gegen Ende 2018 wurde ich auf die De-Broglie-Bohm-Theorie aufmerksam, durch einen Videobeitrag von Josef M. Gaßner,'."\n".
+                    'und begann zu recherchieren.\\footnote{Videobeitrag: \\jump[https://www.youtube.com/watch?v=THu3tWcZnC4]{}{Kollaps der Wellenfunktion: Bohmsche Mechanik, Pilot Wave, Multiversen | AzS (34) | Josef M. Gaßner}.}'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:FrQFT:De-Broglie-Bohm-Theorie:Vortext:XXX', text =>
+                      
+                'Elementarteilchen, die ins Vakuum eingebetet sind, werden durch Wellen im Vakuum beeinflusst', subline =>
+                  'Die Führungswelle beziehungsweise Pilotwelle')),
+                  array( 'text', array( text => array(
+                    'In der hier beschriebenen Quanten-Fluss-Theorie sind die Elementarteilchen ins Vakuum und seine Wellen eingebettet.'."\n".
+                    'Dies ist etwas anders, als in der De-Broglie-Bohm-Theorie, in der die Elementarteilchen von der Führungswelle oder Pilotwelle getrennt erscheinen.XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:FrQFT:De-Broglie-Bohm-Theorie:Vortext:XXX', text =>
+                      
+                'Elapsonen-Fluss-Projektionen im Vakuum', subline =>
+                  'Der Experimentaufbau prägt ein Muster ins Vakuum')),
+                  array( 'text', array( text => array(
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    '\\cite{Struyve:EineKlassischeQuantenwelt:2020}'."\n".
+                    'XXX'."\n"))),
+                  array( 'text', array( text => array(
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -49,6 +123,10 @@
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:FrQFT:De-Broglie-Bohm-Theorie:Vortext:XXX', text =>
+                      
+                'XXX', subline =>
+                  '')),
                   array( 'text', array( text => array(
                     '\\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
@@ -72,7 +150,7 @@
 
 		    	<!  • XXX  >
           <?php $Sc_g_equation_auto_num = 0; ?>
-          <?php /*To_f_Chapter_v1( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '          ', 'invis',
+          <?php /* To_f_Chapter_v1( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '          ', 'invis',
             'OM:FrQFT:De-Broglie-Bohm-Theorie:XXX',
               '\\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
                 array(
@@ -103,7 +181,7 @@
                       //array(  jump_name => 'XXX'),
                     )),
               )
-          );*/ ?>
+          ); */ ?>
 
 
 			<?php FrQFT_f_Div_WrapperEnd( '      '); ?>
