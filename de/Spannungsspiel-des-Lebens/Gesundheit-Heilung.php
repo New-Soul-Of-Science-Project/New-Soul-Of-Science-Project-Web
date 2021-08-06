5c466f57b8b4fdd01f48ccd376261a7bca15da85
Wolfgang Huß <wolle.huss@pjannto.com>
2021-08-06 11:03:30 +0200

    Create Gesundheit-Heilung.php

---
+++ de/Spannungsspiel-des-Lebens/Gesundheit-Heilung.php
@@ -0,0 +1,109 @@
+<?php   $Glo_PathRel_back = '../';
+        include $Glo_PathRel_back.'../share/php/NSOSP.php'; ?>
+
+
+<?php FrQFT_f_HTML_BeginHeaderBodyContainer( 'OM:SpaLeb:XXX'); ?>
+
+
+      <?php FrQFT_f_Div_Header( '      '); ?>
+
+
+      <?php FrQFT_f_Div_WrapperBegin( '      ', 'OM:SpaLeb:XXX'); ?>
+
+	
+          <?php To_f_Paragraph_list( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '                ',
+                array(
+                  array( 'notice', array( Display => 'vis', text => array(
+                    '\\bold{XXX}',
+                    '• XXX',
+                    ))),
+                      
+                  array( 'text', array( text => array(
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:SpaLeb:XXX:Vortext:XXX', text =>
+                      
+                'XXX', subline =>
+                  '')),
+                  array( 'text', array( text => array(
+                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n"))),
+                      
+                  array( 'jumplist', array(
+                      //array(  jump_name => 'OM:SpaLeb:XXX'),
+                    )),
+                )
+          ); ?>
+		    	<br>
+          <?php To_f_headline_add_hides_end_line(); ?>
+          
+
+		    	<!  • XXX  >
+          <?php $Sc_g_equation_auto_num = 0; ?>
+          <?php To_f_Chapter_v1( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '          ', 'invis',
+            'OM:SpaLeb:XXX:XXX',
+              '\\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
+                array(
+                  array( 'jumplist', array(
+                      //array(  jump_name => 'OM:SpaLeb:XXX', type => 'back'),
+                    )),
+                      
+                  array( 'notice', array( Display => 'vis', text => array(
+                    '\\bold{XXX}',
+                    '• XXX',
+                    ))),
+                      
+                  array( 'text', array( text => array(
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:SpaLeb:XXX:Vortext:XXX', text =>
+                      
+                'XXX', subline =>
+                  '')),
+                  array( 'text', array( text => array(
+                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n"))),
+                      
+                  array( 'jumplist', array(
+                      //array(  jump_name => 'OM:SpaLeb:XXX'),
+                    )),
+              )
+          ); ?>
+
+
+			<?php FrQFT_f_Div_WrapperEnd( '      '); ?>
+
+	
+<?php FrQFT_f_HTML_EndDivsNavExtrFootContainerBody(); ?>
