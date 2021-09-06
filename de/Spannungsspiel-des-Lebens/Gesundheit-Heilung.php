fb730a99b36092f58fb58756da2e79f13007e7ac
Wolfgang Huß <wolle.huss@pjannto.com>
2021-09-06 20:49:23 +0200

    Add content "Gesundheit und Heilung"

--- de/Spannungsspiel-des-Lebens/Gesundheit-Heilung.php
+++ de/Spannungsspiel-des-Lebens/Gesundheit-Heilung.php
@@ -66,7 +66,7 @@
           <?php $Sc_g_equation_auto_num = 0; ?>
           <?php To_f_Chapter_v1( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '          ', 'invis',
             'OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf',
-              'Eine Frage der bewussten Kontrolle', 'Sc_f_Paragraph',
+              'Eine Frage des Lösens der bewussten Kontrolle', 'Sc_f_Paragraph',
                 array(
                   array( 'jumplist', array(
                       //array(  jump_name => 'OM:SpaLeb:XXX', type => 'back'),
@@ -78,35 +78,84 @@
                     ))),
                       
                   array( 'text', array( text => array(
-                    'Einer der wesentlichen Unterschiede zwischen \\jump{OM:SpaLeb:Schlaf-und-Aktivsein}{Aktivsein und Schlaf} ist, dass das Aktivsein ein kontrollierter Zustand ist,'."\n".
-                    'während das Schlafen einen Kontrollverlust bedeutet.'."\n"))),
+                    'Einer der wesentlichen Unterschiede zwischen \\jump{OM:SpaLeb:Schlaf-und-Aktivsein}{Aktivsein und Schlaf} ist, dass das Aktivsein ein recht bewusst kontrollierter Zustand ist,'."\n".
+                    'während das Schlafen einen vom Bewusstsein unkontrollierten Zustand bedeutet.'."\n"))),
                   array( 'headline', array( jump_name => 'OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf:HeilungImSchlaf', text =>
                       
                 'Heilung im Schlaf', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    'Dieses Loslassen, dieser Kontrollverlust im Schlaf, ermöglicht es dem Unterbewusstsein uns zu heilen; von unserem Tag und auch generell.'."\n",
+                    'Dieses Loslassen, diese Unkontrolliertheit im Schlaf, ermöglicht es dem Unterbewusstsein uns zu heilen; von unserem Tag und auch generell.'."\n",
                       'Nehmen wir zu viel Kontrolle mit in den Schlaf, indem wir zum Beispiel unsere Gedanken, unsere Sorgen, mit in den Schlaf nehmen,'."\n".
                     'dann wird diese Heilung, diese Regeneration, gestört.'."\n",
                       'Um uns zu regenerieren detektieren wir tief in unseren inneren Zustand hinein.'."\n".
-                    'Das Detektieren gehört zu Regelprozessen, die in Gang gesetzt werden, um zu verarbeiten, aufzuräumen und wieder einen guten Zustand für den nächsten Tag herzustellen.'."\n".
+                    'Das Detektieren oder auch Fühlen gehört zu Regelprozessen, die in Gang gesetzt werden, um zu verarbeiten, aufzuräumen und wieder einen guten Zustand für den nächsten Tag herzustellen.'."\n".
                     'Ich vermute, dass diese Regelprozesse der Verarbeitung und Regeneration nur gut funktionieren, wenn sie nicht von den Handlungsabläufen des Tages und dem für diese'."\n".
                     'notwendigen Fokus „gestört“ werden.'."\n".
-                    'Sie müssen für ein gutes Lernen und eine gute Heilung im Flow sein, worin wir erkennen können, das Lernen und Heilung im Grunde keine zwei Dinge sind.'."\n",
-                      'Einen wichtigen Teil der Verarbeitung, des Lernens, vermute ich in unseren \\jump{OM:SpaLeb:Schlaf-und-Aktivsein}{Träumen}.'."\n".
+                    'Sie müssen für ein gutes Verarbeiten – Lernen – und eine gute Heilung im Flow sein, woran wir erkennen können, das Lernen und Heilung im Grunde keine zwei verschiedenen Dinge sind.'."\n",
+                      'Historisch gesehen werden daher Glaube, Heilung und Persönlichkeitsentwicklung oft in einem Atemzug genannt.'."\n".
+                    'Dieser Zusammenhang war prinzipiell unseren Vorfahren bekannt, wenn auch nicht in einem biophysikalischen Modell wissenschaftlich erklärt.'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf:XXX', text =>
+                      
+                'Unkontrolliertheit im Schlaf erfordert Vertrauen und unseren Glauben', subline =>
+                  '')),
+                  array( 'text', array( text => array(
+                    'Wenn wir schlafen, hat dies mehr mit unserer \\jump{OM:NPYo:Spiritualitaet-Psychologie}{Spiritualität} zu tun, als viele Menschen es gemeinhin annehmen.'."\n",
+                      'Denn die Unkontrolliertheit, die immer eintritt, wenn wir schlafen, erfordert unser Vertrauen in unser positives Schicksal.'."\n".
+                    'Wir können nur in Ruhe und entspannt schlafen und Geborgenheit empfinden, wenn wir glauben, dass uns nichts passiert, während wir „abwesend“ sind.'."\n".
+                    'Unsere Spiritualität und unser Schlaf sind also zwei Seiten der selben Medaille.'."\n",
+                      'Ich meine, schon, wenn ich explizit oder auch nur implizit Glauben ablehne und für „alles“ Beweise brauche, werde ich nicht so tief und erholsam schlafen,'."\n".
+                    'als wenn ich mir bewusst bin, dass guter Schlaf und Heilung auf Vertrauen und Glauben beruhen; denn ich kann die Kontrolle schwer abgeben.'."\n"))),
+                  array( 'text', array( text => array(
+                  '\\condb{Es macht keinen Sinn nicht zu vertrauen und zu glauben} \\\\'."\n".
+                    'Es ist bemerkenswert, dass in dieser Aussage ein Paradoxon erscheint:'."\n".
+                    'Wenn ich glaube – denn ich kann es ja nicht sicher wissen –, ich sei nicht spirituell, dann hat dieser Glaube schon etwas spirituelles.'."\n".
+                    'Das es ohne Vertrauen und damit Glauben nicht geht, erkennen wir schon daran, dass ohne unseren Glauben an eine positive Zukunft'."\n".
+                    'unser Leben keinen Sinn macht.'."\n".
+                    'Dabei sollte klar sein, dass wir nicht sicher unsere positive Zukunft wissen können.'."\n",
+                      'Eine Verneinung meiner Spiritualität muss also zu einem Paradoxon, zu einem Widerspruch, führen.'."\n",
+                      'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf:XXX', text =>
+                      
+                'Gesundungsschlaf', subline =>
+                  '')),
+                  array( 'text', array( text => array(
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX Gesundungsschlaf'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf:XXX', text =>
+                      
+                'Träume', subline =>
+                  '')),
+                  array( 'text', array( text => array(
+                    'Ein wichtiger Teil der Verarbeitung, des Lernens, denke ich liegt in unseren \\jump{OM:SpaLeb:Schlaf-und-Aktivsein}{Träumen}.'."\n".
+                    'Beim Träumen kommunizieren unsere Pole des Bewusstsein und Unterbewusstseins miteinander, wobei das Unterbewusstsein im Vordergrund steht,'."\n".
+                    'weil wir schlafen.'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n",
+                      'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n".
+                    'XXX'."\n"))),
+                  array( 'headline', array( jump_name => 'OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf:XXX', text =>
+                      
+                'Unkontrolliertheit versa Kontrollverlust', subline =>
+                  '')),
                   array( 'text', array( text => array(
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
-                    'XXX Vertrauen, Glauben, \\jump{OM:NPYo:Spiritualitaet-Psychologie}{Spiritualität}'."\n".
+                    'XXX Unkontrolliert sein versa Kontrollverlust, Agonist und Antagonist in flexibler Gegenspannung versa in völligem Loslassen (Erschöpfungsschlaf)'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
