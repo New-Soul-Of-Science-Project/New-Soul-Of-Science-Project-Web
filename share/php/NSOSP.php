377f0a461e2ff3b94e6f84b55b97db8f7c872486
Wolfgang Huß <wolle.huss@pjannto.com>
2021-09-26 13:23:58 +0200

    Set new date for deployment

--- share/php/NSOSP.php
+++ share/php/NSOSP.php
@@ -3498,7 +3498,7 @@
     echo '    <meta name="keywords"           content="'.(To_f_Text_replace_html( $To_g_Text_replace_ary, $To_g_Text_replace_preg_ary, $Glo_g_Site_ary[$Glo_g_Site_activ][keywords])).'">'."\n";
     echo '    <meta name="author"             content="Wolfgang Huss, info@nsosp.org">'."\n";
     echo '    <meta name="DC.Publisher"       content="MEDIA LINE DIGITAL e.K., info@media-line-digital.de">'."\n";
-    echo '    <meta name="DC.Date"            content="2021-05-21T09:00+01:00"><!-- Zeitstempel Beispiel: 01.08.2001, 12:00 Uhr, +1 Std. zu Greenwich -->'."\n";
+    echo '    <meta name="DC.Date"            content="2021-09-26T13:00+01:00"><!-- Zeitstempel Beispiel: 01.08.2001, 12:00 Uhr, +1 Std. zu Greenwich -->'."\n";
     echo '    <meta name="DC.Identifier"      content="'.($Glo_g_Site_ary[$Glo_g_Site_activ][url_abs]).'" scheme="DCTERMS.URI">'."\n";
     echo '    <meta rel="canonical"           href="'.($Glo_g_Site_ary[$Glo_g_Site_activ][url_abs]).'" />'."\n";
     echo '    <meta name="DC.Language"        content="de">'."\n";
@@ -3766,7 +3766,7 @@
     echo $offset.'    <br>'."\n";
     //%!echo $offset.'    <p style="color: #505050">'."\n";
     echo $offset.'    <p>'."\n";
-    echo $offset.'      Stand 21. Mai 2021, 09:00 CET.'."\n";
+    echo $offset.'      Stand 26. September 2021, 13:00 CET.'."\n";
     echo $offset.'    </p>'."\n";
     echo $offset.'    <br>'."\n";
     //%!echo $offset.'    <p style="font-size: 13px; line-height: 0.7em;">'."\n";
