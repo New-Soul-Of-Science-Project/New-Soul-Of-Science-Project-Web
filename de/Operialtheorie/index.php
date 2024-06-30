6c535072042c3d25dd10ad65453ff3f1f0bd4195
Wolfgang Huß <wolle.huss@pjannto.com>
2024-06-30 12:05:01 +0200

    Refine home - theme OT

--- de/Operialtheorie/index.php
+++ de/Operialtheorie/index.php
@@ -18,8 +18,8 @@
                     'Die Addition, die Multiplikation und die Exponentialrechnung gehen auf gleiche Weise auseinander hervor;'."\n".
                     'vom ersten Operator Plus „\lm{ + }“ über den zweiten Operator Mal „\lm{ \cdot }“ bis zum dritten Operator Hoch „\lm{ ↑ }“.'."\n".
                     'Dadurch werden diese Operator-Arten zählbar und wir können über diese Systematik weitere finden.'."\n",
-                      'Es sich lohnt sich aus verschiedenen Gründen, uns hiermit zu beschäftigen.'."\n",
-                      'Wir ermitteln so den Vorgänger-Operator der Addition und dieser ist tatsächlich das Zählen selber.'."\n".
+                      'Aus verschiedenen Gründen lohnt es sich, uns hiermit zu beschäftigen.'."\n",
+                      'Wir ermitteln so den Vorgängeroperator der Addition und dieser ist tatsächlich das Zählen selber.'."\n".
                     'Er kann uns zum Beispiel etwas über die Zeit lehren und über Existenz,'."\n".
                     'denn wir können ihn so interpretieren, dass der gegenwärtige Zähl- oder Zeitschritt nur dann konstant bleibt,'."\n".
                     'wenn der nächste Schritt noch nicht existiert.'."\n".
@@ -30,9 +30,10 @@
                     'dann wird die rechte Zahl genommen und um einen erhöht.'."\n".
                     'Wir können mit seiner Hilfe aus Nicht-Existierendem mit einer Existenz eine Existenz generieren oder schöpfen.'."\n",
                       'Ähnliches gilt für den nächsten Vorgänger, den Konstanzoperator.'."\n",
-                      'Wir kommen also durch genaue Beobachtung der Zahl Operatoren und ihrer Vorgänger prinzipiell zum Existenzquantor\footnote{\color{*Bearb}{(Primärliteratur einfügen!)} \\\\ Internet: \\\\ Vgl. \cite{wiki:Quantor:2023}, Existenz- und Allquantor.} „\lm{ \exists }“.'."\n".
-                    'Und ich halte es für möglich, dass so auch prinzipiell der Allquantor\footnote{\color{*Bearb}{(Primärliteratur einfügen!)} \\\\ Internet: \\\\ Vgl. \cite{wiki:Quantor:2023}, Existenz- und Allquantor.} „\lm{ \forall }“ auf Basis der Zahl Operatoren zu formulieren ist.'."\n".
-                    'Wir können also sogar zunächst unvermutete Zusammenhänge mit der Prädikatenlogik\footnote{\color{*Bearb}{(Primärliteratur einfügen!)} \\\\ Internet: \\\\ Vgl. \cite{wiki:Praedikatenlogik:2023}.} erkennen, die auf mehr neugierig machen.'."\n".
+                      'Wir kommen also durch genaue Beobachtung der Zahl-Operatoren und ihrer Vorgänger prinzipiell zum Existenzquantor\footnote{\color{*Bearb}{(Primärliteratur einfügen!)} \\\\ Internet: \\\\ Vgl. \cite{wiki:Quantor:2023}, Existenz- und Allquantor.} „\lm{ \exists }“.'."\n".
+                    'Und ich denke, dass so auch prinzipiell der Allquantor\footnote{\color{*Bearb}{(Primärliteratur einfügen!)} \\\\ Internet: \\\\ Vgl. \cite{wiki:Quantor:2023}, Existenz- und Allquantor.} „\lm{ \forall }“ auf Basis der Zahl-Operatoren zu formulieren ist.'."\n".
+                    'Es zeigen sich also auf diese Weise sogar zunächst verborgene Zusammenhänge mit der Prädikatenlogik\footnote{\color{*Bearb}{(Primärliteratur einfügen!)} \\\\ Internet: \\\\ Vgl. \cite{wiki:Praedikatenlogik:2023}.},'."\n".
+                    'die die fundamentale Bedeutung der Operialtheorie erkennen lassen und auf mehr neugierig machen.'."\n".
                     ''))),
                       
                   array( 'text', array( Shape => 'italic', text => array(
