deabd13e6de98f5c72c45b62ae59f54c3c099b4e
Wolfgang Huß <wolle.huss@pjannto.com>
2024-12-17 22:54:17 +0100

    Replace '\\<command>' by '\<command>'

--- de/Spannungsspiel-des-Lebens/Diskussion_de.php
+++ de/Spannungsspiel-des-Lebens/Diskussion_de.php
@@ -19,7 +19,7 @@
                     ))),
                       
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{(In Arbeit …)}'."\n".
+                    '\color{*Bearb}{(In Arbeit …)}'."\n".
                     ''))),
                       
                   array( 'jumplist', array(
@@ -35,7 +35,7 @@
           <?php $Sc_g_equation_auto_num = 0; ?>
           <?php To_f_Chapter_v1( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '          ', 'hideContent',
             'OM:SpaLeb:Diskussion:Achtsamkeit',
-              '\\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
+              '\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
                 array(
                   array( 'jumplist', array(
                       array(  jump_name => 'OM:SpaLeb:Diskussion', type => 'back'),
@@ -44,7 +44,7 @@
                   array( 'notice', array( Display => 'showContent', text => array(
                     '(• Weiter entwickeln und dabei sammeln, was Achtsamkeit noch alles bedeutet.)',
                     '(• Unterschied vom Achtsamkeitsprozess zur Achtsamkeit: So wie viele Menschen die Achtsamkeit mir heute zu verstehen scheinen, tun sich wichtige Unterschiede zu meinem Verständnis vom Achtsamkeitsprozess auf: Bei der Achtsamkeit scheint mir im Vordergrund zu stehen, dass man auf sich, seine Mitmenschen und seine Umwelt acht geben soll, gut zu sich selber, zu seinen Mitmenschen und seiner Umwelt sein und sich verhalten soll. Ich würde auch grundsätzlich zustimmen, dass dies in gewisser Hinsicht erstrebenswert ist. Doch was ist sich gut verhalten? Der Achtsamkeitsprozess ist dazu da herauszufinden, was ein Sich-Gut-Verhalten in diesem Sinne für jeden von uns individuell ist. Nämlich ein Verhalten, was mich selber in einem sehr grundsätzlichen Sinne bei der Handlung und danach wohl fühlen lässt. Um herauszufinden wie ich funktioniere, wie mein Leben funktioniert, was mich wohl fühlen lässt, muss es mir auch mal nicht so gut gehen: Ich muss mich mal besaufen, um zu fühlen, dass ich hinterher einen Kater habe; und das muss auch nicht bedeuten, dass ich es nie wieder tue. Wichtig ist, dass ich es deutlich und bewusst fühle und lerne, dass dem so ist und ich daraus auch meine Schlüsse ziehe. Es geht also darum Dinge zu tun und zu merken, wie ich mich damit fühle. Mir scheint die Achtsamkeit in dieser Beziehung zu einseitig: Hier wird sehr viel Augenmerk darauf gelegt, möglichst nur Gutes zu tun. Doch wie soll ich dann lernen, was nicht so gut ist und wie ich mich fühle, wenn ich Dinge tue, die nicht so gut sind. Ich muss ja nicht gleich extrem schlechte Dinge tun. Nur, wenn ich mich nur auf die guten Dinge fokussiere kann, mein Achtsamkeitsprozess sich nicht so gut justieren und wird dann auch nicht so gut funktionieren. Ich muss hier und dort auch ins Risiko gehen und dabei auch mal verletzt werden, Blessuren davon ziehen. Dann fühle ich sehr deutlich, worum es geht.)',
-                    '(• \\quote{Achtsamkeit bedeutet, die eigenen Gedanken auf den Moment zu konzentrieren.} \\quote{Das bedeutet, den Moment zu erleben, ohne ihn zu bewerten oder sich gefühlsmäßig davon fortreißen zu lassen.} \\quote{Achtsamkeit ist ein Zustand, in dem wir unsere Gedanken vollkommen auf das Hier und Jetzt konzentrieren – ohne darüber zu urteilen oder emotional darauf zu reagieren.} Siehe Spektrum Kompakt. Achtsamkeit und Empathie. 04.07.2016. Augenblick mal! (aus Gehirn und Geist, 11/2013) S. 4-12, hier S. 5. \\quote{Ein ganzes Arsenal von Übungen soll die Aufmerksamkeit stärker auf konkrete Wahrnehmungen des Hier und Jetzt lenken und so zu mehr Klarsicht und Ruhe verhelfen.} Ebenda, hier S. 6.)',
+                    '(• \quote{Achtsamkeit bedeutet, die eigenen Gedanken auf den Moment zu konzentrieren.} \quote{Das bedeutet, den Moment zu erleben, ohne ihn zu bewerten oder sich gefühlsmäßig davon fortreißen zu lassen.} \quote{Achtsamkeit ist ein Zustand, in dem wir unsere Gedanken vollkommen auf das Hier und Jetzt konzentrieren – ohne darüber zu urteilen oder emotional darauf zu reagieren.} Siehe Spektrum Kompakt. Achtsamkeit und Empathie. 04.07.2016. Augenblick mal! (aus Gehirn und Geist, 11/2013) S. 4-12, hier S. 5. \quote{Ein ganzes Arsenal von Übungen soll die Aufmerksamkeit stärker auf konkrete Wahrnehmungen des Hier und Jetzt lenken und so zu mehr Klarsicht und Ruhe verhelfen.} Ebenda, hier S. 6.)',
                     ))),
                       
                   array( 'text', array( text => array(
@@ -57,8 +57,8 @@
                     'Der Achtsamkeitsprozess bezieht sich wirklich nur auf das Wechselspiel der Selbstwahrnehmung, und im Besonderen des Sich-Selber-Spürens und -Fühlens, und seinem eigenen Handeln,'."\n".
                     'im Hinblick darauf, was das Handeln mit einem macht.'."\n".
                     'Dies hat zur Folge, das man allgemein Achtsamer wird und auch die Blumen am Wegesrand besser und deutlicher wahrnimmt.'."\n".
-                    'Doch dies ist kein Teil des \\italic{einfachen Achtsamkeitsprozesses}.'."\n".
-                    'Der \\italic{externe Achtsamkeitsprozess} bezieht das Wechselspiel zwischen einem selbst und der Umwelt mit ein.'."\n".
+                    'Doch dies ist kein Teil des \italic{einfachen Achtsamkeitsprozesses}.'."\n".
+                    'Der \italic{externe Achtsamkeitsprozess} bezieht das Wechselspiel zwischen einem selbst und der Umwelt mit ein.'."\n".
                     'Doch auch nur im Hinblick auf das eingene Handeln, nun inklusive der Wahrnehmung der Umwelt, und was dies mit der Umwelt und darüber mit einem macht.'."\n".
                     'Wie es das eigene Gefühl erzeugt und verändert.'."\n".
                     'Dabei ist die Wahrnehmung der Umwelt eben Teil des Handelns.'."\n".
