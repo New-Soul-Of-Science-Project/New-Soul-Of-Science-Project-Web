b9492b90b3931f59653f0bada12666d3f1565d0e
Wolfgang Huß <wolle.huss@pjannto.com>
2024-12-21 20:20:12 +0100

    Replace '\\<command>' by '\<command>'

--- de/Spannungsspiel-des-Lebens/Schlaf-und-Aktivsein-Unterbewustsein-und-Bewusstsein.php
+++ de/Spannungsspiel-des-Lebens/Schlaf-und-Aktivsein-Unterbewustsein-und-Bewusstsein.php
@@ -14,34 +14,34 @@
           <?php To_f_Paragraph_list( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '                ',
                 array(
                   array( 'notice', array( Display => 'hideContent', text => array(
-                    '\\bold{Biologischer Bezug}',
+                    '\bold{Biologischer Bezug}',
                     '• Die weibliche Gefühlsseite unseres Achtsamkeitsprozesses ist mit unserem Parasympathikus, dem parasympathischen Teil unseres zentralen Nervensystems verknüpft.',
                     '– Wird unser Parasympathikus aktiviert, dann beruhigen wir uns vornehmlich, bis in den Schlaf.',
                     '• Die männliche Handlungsseite unseres Achtsamkeitsprozesses ist mit unserem Sympathikus, dem sympathischen Teil unseres zentralen Nervensystems verknüpft.',
                     '– Wird unser Sympathikus aktiviert, dann werden wir wach und aktiv und kommen in Handlung.',
-                    '• \\italic{Unser Schlaf:} Schlafen wir, dann sind wir in einem vorwiegend unterbewussten Zustand (siehe auch \\jumpname{OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf:HeilungImSchlaf}). Sind wir wach, dann sind wir vorwiegend bewusst. Unser Leben schwankt also generell zwischen diesen beiden Polen unseres Achtsamkeitsprozesses im Laufe des Tages hin und her. Beim Bewusstsein und beim Unterbewusstsein handelt es sich also auch um zwei sich ergänzende, also komplementäre Pole. Wir können hieran erkennen, dass unser Schlaf fundamental mit unserer Existenz, mit unserem Leben, mit unserem Achtsamkeitsprozess verknüpft ist. Unser Schlaf ist die eine Hälfte unseres Lebens unser Aktivsein die andere. Heute wird der Schlaf von vielen (westlichen) Menschen als störend empfunden, als etwas, das sie von ihrem Leben abhält, und tendenziell abgelehnt. Viele Menschen empfinden ihren Schlaf als ineffektive Zeit. Dies ist nach meiner Interpretation eine vollkommene Fehleinschätzung und ein schwerwiegendes Problem.',
+                    '• \italic{Unser Schlaf:} Schlafen wir, dann sind wir in einem vorwiegend unterbewussten Zustand (siehe auch \jumpname{OM:SpaLeb:Gesundheit-Heilung:HeilungSchlaf:HeilungImSchlaf}). Sind wir wach, dann sind wir vorwiegend bewusst. Unser Leben schwankt also generell zwischen diesen beiden Polen unseres Achtsamkeitsprozesses im Laufe des Tages hin und her. Beim Bewusstsein und beim Unterbewusstsein handelt es sich also auch um zwei sich ergänzende, also komplementäre Pole. Wir können hieran erkennen, dass unser Schlaf fundamental mit unserer Existenz, mit unserem Leben, mit unserem Achtsamkeitsprozess verknüpft ist. Unser Schlaf ist die eine Hälfte unseres Lebens unser Aktivsein die andere. Heute wird der Schlaf von vielen (westlichen) Menschen als störend empfunden, als etwas, das sie von ihrem Leben abhält, und tendenziell abgelehnt. Viele Menschen empfinden ihren Schlaf als ineffektive Zeit. Dies ist nach meiner Interpretation eine vollkommene Fehleinschätzung und ein schwerwiegendes Problem.',
                     '⋅ So ist nun klar, warum alle Tiere schlafen, obwohl dies auch Risiken birgt. Unser Schlaf muss eine große Bedeutung haben, die wir bisher offenbar nur sehr bedingt verstanden haben. Er ist unsere vollständig introspektive Phase des Tages, die mit unseren Träumen und vermutlich noch mit anderen, sehr bedeutsamen Dingen verbunden ist.',
-                    '• Unser Bewusstsein scheint mir mit unserem Handlungspol verbunden und unser Unterbewusstsein mit unserem Gefühlspol (siehe \\italic{\\jumpname{OM:NPYo:Spiritualitaet-Psychologie}}).',
+                    '• Unser Bewusstsein scheint mir mit unserem Handlungspol verbunden und unser Unterbewusstsein mit unserem Gefühlspol (siehe \italic{\jumpname{OM:NPYo:Spiritualitaet-Psychologie}}).',
                     '– Unsere Intuition entsteht aus dem Dialog unseres Bewusstseins mit unserem Unterbewusstsein, wenn das Bewusstsein im Vordergrund steht.',
                     '– Unsere Träume sind unser Dialog unseres Bewusstseins mit unserem Unterbewusstsein, wenn unser Unterbewusstsein im Vordergrund steht.',
-                    '• Die Verbindung unserer körperlichen Gefühlsseite, unseres Gefühlspols, mit unserem Unterbewussten und unserer körperlichen Handlungsseite, unseres Handlungspols, mit unserem Bewusstsein zeigt sich in Folgendem (siehe auch \\italic{\\jumpname{OM:SpaLeb:Care-Prozess:BeobachtHinweise}}):',
+                    '• Die Verbindung unserer körperlichen Gefühlsseite, unseres Gefühlspols, mit unserem Unterbewussten und unserer körperlichen Handlungsseite, unseres Handlungspols, mit unserem Bewusstsein zeigt sich in Folgendem (siehe auch \italic{\jumpname{OM:SpaLeb:Care-Prozess:BeobachtHinweise}}):',
                     '– Unser Herz, dessen Schlag wir nicht bewusst steuern können, liegt auf unserer körperlichen Gefühlsseite des Unterbewussten, dafür ist auf dieser Seite unsere Lunge verkleinert. Wir können also sagen, dass unsere Atmung sowohl auf unserer unterbewussten Seite, als auch auf unserer bewussten Seite, mit dem leichten Schwerpunkt in Richtung Bewusstsein, verortet ist. So in etwa beobachten wir auch unsere Verbindung zu unserer Atmung.',
                     '• Hierin lässt sich das hierarchisch fraktale, selbstähnliche Wesen unseres Achtsamkeitsprozesses erkennen:',
                     '– Bezeichne ich unsere weibliche Gefühlsseite als unsere passive Seite, dann können wir sagen: Wenn wir ganz in Ruhe und beständig unsere passive Seite aktivieren (in Ruhe → aktivieren → passive Seite), dann kommen wir zur Ruhe, bis in unseren Schlaf. Unser Schlaf ist also mit einer fraktal wechselnden Hierarchie von passiv, aktiv, passiv, und so fort, verbunden.',
                     '– Bezeichne ich unsere männliche Handlungsseite als unsere aktive Seite, dann können wir sagen: Wenn wir aktiv und beständig unsere aktive Seite aktivieren (aktiv → aktivieren → aktive Seite), wir erwachen und kommen wir in Aktion. Unsere Aktion oder starke Handlung ist also mit einer fraktal gleichen Hierarchie von aktiv, aktiv, aktiv, und so fort, verbunden.',
-                    '\\bold{Bewusstsein}',
-                    '• Siehe auch \\italic{\\jumpname{OM:SpaLeb:Bewusstsein}}.',
-                    '\\bold{Dualität von und Dialog zwischen Bewusstsein und Unterbewusstsein}',
+                    '\bold{Bewusstsein}',
+                    '• Siehe auch \italic{\jumpname{OM:SpaLeb:Bewusstsein}}.',
+                    '\bold{Dualität von und Dialog zwischen Bewusstsein und Unterbewusstsein}',
                     '• Eigene Seite?',
                     '• Bewusstsein steht für Wissen, für Licht, für Weisheit (weiße Haare) und Unterbewusstsein oder Unbewussten steht für Unwissen, für Dunkelheit, für Unbestimmtheit und auch für Potenzial.',
                     '• Wir schöpfen aus dem Unterbewusstsein ins Bewusstsein, wir lernen, wir bringen Dinge zur Existenz und realisieren sie. Dies geschieht in unserem Dialog zwischen Bewusstsein und Unterbewusstsein.',
-                    '• Bezug zu den \\italic{\\jump{OM:SupNum:Home}{Superial-Zahlen}}:',
-                    '– \\term{s} steht für das Potenzial, den Raum der Möglichkeiten.',
-                    '– Zählen wir die natürlichen Superial-Zahlen von Null bis \\term{s}, dann zählen wir übers Endliche hinaus, über einen unbekannten „dunklen“ Bereich, bis ins Unendliche und kommen dort bei der unendlichen Eins (\\term{1⋅s}) wieder heraus. Dazwischen liegen unbewusste, verborgene Welten – Mysterien.',
-                    '\\bold{Historischer Bezug}',
+                    '• Bezug zu den \italic{\jump{OM:SupNum:Home}{Superial-Zahlen}}:',
+                    '– \term{s} steht für das Potenzial, den Raum der Möglichkeiten.',
+                    '– Zählen wir die natürlichen Superial-Zahlen von Null bis \term{s}, dann zählen wir übers Endliche hinaus, über einen unbekannten „dunklen“ Bereich, bis ins Unendliche und kommen dort bei der unendlichen Eins (\term{1⋅s}) wieder heraus. Dazwischen liegen unbewusste, verborgene Welten – Mysterien.',
+                    '\bold{Historischer Bezug}',
                     '• In dem oben geschilderten Kontext ist nun zu verstehen, warum unsere weibliche Gefühlsseite in früheren Kulturen, Religionen und Mythen mit dem Mond identifiziert wurde und unsere männliche Handlungsseite mit der Sonne. Der Schlaf und unser Unterbewusstsein spielten dort eine große und wichtige Rolle.',
                     '• Die bisher zu wenig erkannten und gewürdigten Aspekte der Bedeutung unseres Schlafs und Unterbewusstseins können wir sicher auch in früheren Kulturen finden, denn diese Menschen hatten einen ganz anderen Blick auf ihr Leben und auf ihren Schlaf und ihr Unterbewusstsein als wir.',
-                    '\\bold{Philosophie}',
+                    '\bold{Philosophie}',
                     '• In letzter Zeit ist mir klar geworden, dass es vielmehr um das Verhältnis vom Bewussten zum Unterbewussten oder Nichtbewussten geht. Es geht darum, was wir wissen und was wir nicht wissen. Und dies ist ein dynamischer Prozess:',
                     '– Das Einzige, was wir sicher intuitiv, und unter Annahme einer bestimmten Struktur der Realität auch logisch, wissen ist, dass wir niemals alles wissen werden.',
                     '– Doch was wir einmal wissen werden oder niemals wissen können, das können wir nicht wissen.',
@@ -50,15 +50,15 @@
                     '– Mit konkreten Dingen, die wir angeblich niemals wissen können, verbietet es sich daher um die Ecke zu kommen. Denn das können wir nicht wissen, dass wir genau diese Dinge niemals wissen werden.',
                     '– Daher sind die Begriffe Unendlichkeit und Absolutheit ganz sicher in diesem Sinne fehl am Platz. In dem Moment, wo wir sie gebrauchen, sind sie schon zu konkret, um nicht strittig zu sein.',
                     '– Daher sind sie als Platzhalter für das zu verstehen, was wir nicht wissen.',
-                    '– \\jump{OM:NPYo:Spiritualitaet-Psychologie}{Spiritualität} ist in meinen Augen dann übrigens unser Glaube an einen fruchtbaren und hilfreichen Dialog unseres Bewusstseins mit unserem Unterbewusstsein oder mit Gott oder mit dem Universum oder dem Vakuum oder wie immer wir das, was wir nicht wissen oder kennen, bezeichnen und natürlich dann auch unsere Praxis dieses Dialogs.',
+                    '– \jump{OM:NPYo:Spiritualitaet-Psychologie}{Spiritualität} ist in meinen Augen dann übrigens unser Glaube an einen fruchtbaren und hilfreichen Dialog unseres Bewusstseins mit unserem Unterbewusstsein oder mit Gott oder mit dem Universum oder dem Vakuum oder wie immer wir das, was wir nicht wissen oder kennen, bezeichnen und natürlich dann auch unsere Praxis dieses Dialogs.',
                     ))),
                       
                   array( 'text', array( text => array(
                     'Unser Leben vollzieht sich in einer dem Tag- und Nachtrhythmus folgenden Schwingung zwischen Wachsein sowie Aktivsein und Schlaf.'."\n".
-                    'Es ist eine Tag-Nacht-Schwingung zwischen den Polen unseres \\jump{OM:SpaLeb:Care-Prozess}{Care-Prozesses},'."\n".
+                    'Es ist eine Tag-Nacht-Schwingung zwischen den Polen unseres \jump{OM:SpaLeb:Care-Prozess}{Care-Prozesses},'."\n".
                     'die Tagsüber meist unseren Handlungspol in den Vordergrund bringt und des Nachts unseren Gefühlspol;'."\n".
                     'eine Schwingen zwischen unserem Bewusstsein und unserem Unterbewusstsein, Unbewusstsein oder Nichtbewusstsein.'."\n",
-                      'Die Realität dieser Schwingung zwischen den Polen unseres Care-Prozesses ist ein weiterer deutlicher Hinweis auf \\jump{OM:SpaLeb:Care-Prozess:BeobachtHinweise}{seinen Realitätsbezug}'."\n".
+                      'Die Realität dieser Schwingung zwischen den Polen unseres Care-Prozesses ist ein weiterer deutlicher Hinweis auf \jump{OM:SpaLeb:Care-Prozess:BeobachtHinweise}{seinen Realitätsbezug}'."\n".
                     'und seine Bedeutung für unser Leben.'."\n".
                     'Unsere dauerhafte Ignoranz dieses Rhythmus ist mit unserem Leben nicht vereinbar.'."\n".
                     'Sie ist tödlich.'."\n",
@@ -88,7 +88,7 @@
                 'Intuition meint, unbewusstes kommt uns bewusst zu Bewusstsein', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -103,7 +103,7 @@
                 'Unterbewusstsein ist nicht bewusstes Bewusstsein', subline =>
                   'Wahrnehmung, die nicht direkt in unser bewusstes Bewusstsein vordringt')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -118,7 +118,7 @@
                 'Schlaf bedeutet, unser Unterbewusstsein ist im Vordergrund', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -133,7 +133,7 @@
                 'Träume sind unser Dialog zwischen Unterbewusstsein und Bewusstsein, während unser Unterbewusstsein im Vordergrund ist', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -148,7 +148,7 @@
                 'Wir schöpfen Wissen aus unserem Unterbewusstsein in unser Bewusstsein', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -163,7 +163,7 @@
                 'XXX', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -178,7 +178,7 @@
                 'XXX', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
@@ -202,7 +202,7 @@
           <?php $Sc_g_equation_auto_num = 0; ?>
           <?php /* To_f_Chapter_v1( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '          ', 'hideContent',
             'OM:SpaLeb:Schlaf-und-Aktivsein:X',
-              '\\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
+              '\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
                 array(
                   array( 'jumplist', array(
                       // array(  jump_name => 'OM:SpaLeb:XXX', type => 'back'),
@@ -228,7 +228,7 @@
                 'XXX', subline =>
                   '')),
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{In Arbeit …}'."\n".
+                    '\color{*Bearb}{In Arbeit …}'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
                     'XXX'."\n".
