8271814458c961dea432052e20ac733c95aa385d
Wolfgang Huß <wolle.huss@pjannto.com>
2026-04-06 15:56:34 +0200

    Replace '\\<command>' by '\<command>'

--- de/Spannungsspiel-des-Lebens/Kuenstliche-Inteligenz-Robotik_de.php
+++ de/Spannungsspiel-des-Lebens/Kuenstliche-Inteligenz-Robotik_de.php
@@ -14,29 +14,29 @@
           <?php To_f_Paragraph_list( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '                ',
                 array(
                   array( 'notice', array( Display => 'showContent', text => array(
-                    '\\bold{Vertrauensproblem:}',
+                    '\bold{Vertrauensproblem:}',
                     '• Ein sehr großes Problem, was wir mit KI vermutlich erleben werden ist ein Vertrauensproblem.',
                     '• Dies gilt ganz besonders für Roboter. Der Mensch ist so evolutionär konstruiert, dass er durch seine Körpersprache, seine Stimme und ganz besonders über sein Gesicht etwas über sich selbst verrät, was er nicht bewusst und schon gar nicht kurzfristig steuern kann. Dies hat die Evolution nicht zufällig eingefädelt!',
-                    '• Unsere Stimmung, unsere Psyche, unsere Lebensführung, unsere Qualitäten, wie unsere Vertrauenswürdigkeit, sind über unseren \\jump{OM:SpaLeb:Care-Prozess}{Care-Prozess} – auch den \\jump{OM:SpaLeb:Care-Prozess:Achtsamkeitsprozess}{Achtsamkeitsprozess} – und seine \\jump{OM:SpaLeb:Psychosomatik-Stress}{Psychosomatik} tief in den \\jump{OM:SpaLeb:Gesichtsausdruck}{Ausdruck unseres Gesichts}, in unsere Körpersprache und in unsere \\jump{OM:SpaLeb:Gesangstechnik}{Stimme} eingeprägt.',
+                    '• Unsere Stimmung, unsere Psyche, unsere Lebensführung, unsere Qualitäten, wie unsere Vertrauenswürdigkeit, sind über unseren \jump{OM:SpaLeb:Care-Prozess}{Care-Prozess} – auch den \jump{OM:SpaLeb:Care-Prozess:Achtsamkeitsprozess}{Achtsamkeitsprozess} – und seine \jump{OM:SpaLeb:Psychosomatik-Stress}{Psychosomatik} tief in den \jump{OM:SpaLeb:Gesichtsausdruck}{Ausdruck unseres Gesichts}, in unsere Körpersprache und in unsere \jump{OM:SpaLeb:Gesangstechnik}{Stimme} eingeprägt.',
                     '• Wir sind als soziale Wesen im sozialen Miteinander davon abhängig, möglichst gut und rechtzeitig zu erkennen, was das Gegenüber im Schilde führt; wer er, sie oder es ist.',
                     '• Und genau diese zwangsweise Verbindung gibt es bei Robotern und KI nicht! Wir können niemals sicher sein, was im nächsten Moment passieren wird. Erleben wir ein völlig unvorhergesehenes, vielleicht sogar lebensbedrohendes Verhalten gegenüber anderen oder gar gegenüber uns selber, werden wir kaum je wieder vertrauen fassen können.',
                     '• Selbst, wenn die Roboter und die KI gut funktionieren, können sie unbemerkt von dritter Seite gehackt werden. Dies ist mit Menschen nur sehr schwer möglich, ohne dass wir mitbekommen, dass etwas komisch ist. Aber natürlich schaffen auch Menschen es, uns zu täuschen. Aber wir haben eine gute Chance es zu merken und vor allem daraus etwas zu lernen! Und genau dies geht bei Robotern und KI vermutlich nur sehr schwer.',
-                    '\\bold{Analyse:}',
+                    '\bold{Analyse:}',
                     '• Der Gödelsche Unvollständigkeitssatz zeigt: „Jedes hinreichend mächtige, rekursiv aufzählbare formale System ist entweder widersprüchlich oder unvollständig.“, siehe Wikipedia',
                     '– Diese Unvollständigkeit zeigt sich im Besonderen in selbstbezüglichen Sätzen.',
                     '– Der Mensch, seine Gedanken und sein Verstand, wie die ganze Natur, denke ich, ist in sich hochgradig selbstbezüglich.',
                     '– Die Grundlage der KI ist mathematische Informatik, die nach meinem Verständnis dem Gödelschen Unvollständigkeitssatz unterliegt.',
                     '– Dieses Verständnis legt sehr nahe, dass die KI nicht in der Lage ist, den menschlichen Verstand und das menschliche Wesen in sehr hoher Güte zu simulieren.',
-                    '\\bold{Aktuelle Fachdiskussion:}',
+                    '\bold{Aktuelle Fachdiskussion:}',
                     '• Artikel aus der Süddeutschen vom 20.07.2018.',
                     '• Artikel aus dem Spektrum der Wissenschaft, Online, vom vom 02.07.2018, »Intelligenz braucht einen Körper«.',
-                    '• Lernen von Robotern und Kleinkindern: Roboter lernen am Besten, wenn sie die Vorhersagefehler minimieren. Theorie der prädiktiven Informationsverarbeitung (predictive proccessing). Ein rückgekoppeltes neuronales Netzwerk, wo die Differenz zwischen Vorhersage und deren realen Überprüfung als Fehlerdiffereznz wieder in die Verarbeitung der Informationsschichten eingespeist wird. Durch das Nachbilden des Lernens von Kleinkindern mit Robotern verstehen wir besser, wie Kleinkinder lernen, welche Probleme dabei auftreten können und wie wir diese vermeiden können.\\footnote{Vgl. \\cite{Kwon:SelbststaendigLernendeRoboter:2018}.}',
+                    '• Lernen von Robotern und Kleinkindern: Roboter lernen am Besten, wenn sie die Vorhersagefehler minimieren. Theorie der prädiktiven Informationsverarbeitung (predictive proccessing). Ein rückgekoppeltes neuronales Netzwerk, wo die Differenz zwischen Vorhersage und deren realen Überprüfung als Fehlerdiffereznz wieder in die Verarbeitung der Informationsschichten eingespeist wird. Durch das Nachbilden des Lernens von Kleinkindern mit Robotern verstehen wir besser, wie Kleinkinder lernen, welche Probleme dabei auftreten können und wie wir diese vermeiden können.\footnote{Vgl. \cite{Kwon:SelbststaendigLernendeRoboter:2018}.}',
                     '– Der Achtsamkeitsprozess scheint universal zu sein: Solche Roboter sind Kleinkindern scheinbar so ähnlich, dass für sie die gleichen Voraussetzungen gelten, wenn sie so wie Kleinkinder werden sollen. »›Wenn mich jemand fragt, ob ein Roboter wirklich Menschenähnlich werden kann, dann frage ich zurück, ob sich jemand um ihn, wie um ein echtes Kind kümmern würde. Wenn ja, dann ist das vielleicht realisierbar; wenn nein, dann kann sich ein Roboter unmöglich wie ein richtiges menschliches Kind entwickeln.‹«, S. 72.',
                     '– Feynman sagt dazu: »›Was ich nicht erschaffen kann, verstehe ich nicht.‹« Jun Tani sagt dazu: »›Ich kann verstehen, was ich erschaffen kann.‹«, S. 72.',
                     ))),
                       
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{(In Arbeit …})'."\n".
+                    '\color{*Bearb}{(In Arbeit …})'."\n".
                     ''))),
                       
                   array( 'jumplist', array(
@@ -51,7 +51,7 @@
 		    	<!  • X  >
           <?php /* To_f_Chapter_v1( $Sc_g_Text_replace_ary, $Sc_g_Text_replace_preg_ary, '          ', 'hideContent',
             'OM:SpaLeb:KIundRobotik:X',
-              '\\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
+              '\color{*Bearb}{In Arbeit …}', 'Sc_f_Paragraph',
                 array(
                   array( 'jumplist', array(
                       // array(  jump_name => 'OM:SpaLeb:KIundRobotik:AchtsamkeitsprozesGrundlLeben', type => 'back'),
@@ -62,7 +62,7 @@
                     ))),
                       
                   array( 'text', array( text => array(
-                    '\\color{*Bearb}{(In Arbeit …)}'."\n".
+                    '\color{*Bearb}{(In Arbeit …)}'."\n".
                     ''))),
                       
                   array( 'jumplist', array(
